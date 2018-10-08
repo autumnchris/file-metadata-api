6d34da3f62802a858bb79b9631c94ef1ccdd68d1
Autumn <autumncbullard@gmail.com>
2018-10-08 18:04:27 -0400

    Make a POST request that returns JSON data when the form is submitted

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
 
+const upload = multer().single('upfile');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,5 +10,13 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static(`${__dirname}/public`));
+
+app.post('/api/fileanalyse', upload, (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
